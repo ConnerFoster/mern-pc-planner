702534bde871f4c22cf2feaf17f1890734b10a65
Conner Foster <connerf99@gmail.com>
2022-07-16 21:40:04 -0400

    add dummy data to db

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const cors = require('cors')
 require('dotenv').config({path: './config.env'})
 const port = process.env.PORT || 5000
 
+const Category = require('./models/category')
+const categoriesRouter = require('./routes/categories')
+
 app.get('/', (req, res, next) => {
   res.end('hello world')
 })
@@ -12,6 +15,8 @@ app.use(cors())
 app.use(express.json())
 //app.use(require('./routes/record'))
 
+app.use('/categories', categoriesRouter)
+
 const mongoose = require('mongoose')
 const mongoDB = process.env.ATLAS_URI
 
@@ -19,6 +24,16 @@ mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'Error connecting to MongoDB: '))
+
+const test = new Category({
+  title: 'CPU',
+  description: 'central processing unit, testing ',
+})
+
+test.save((err, test) => {
+  if (err) return console.error(err)
+  console.log(test.title + ' saved to db')
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
